7bdbb763f90f6d7f0390a2b70bc6617a0d7d73d7
Vo Duc Huy <voduchuy2001@gmail.com>
2025-05-12 09:39:45 +0700

    update: validate

--- src/components/contact/validation.jsx
+++ src/components/contact/validation.jsx
@@ -1,7 +1,15 @@
 import { z } from 'zod'
 
 export const contactSchema = z.object({
-  name: z.string().min(1, 'Name is required'),
-  email: z.string().email('Must be type of email'),
-  message: z.string().min(1, 'Message is required')
+  name: z
+    .string({ required_error: 'Name is required' })
+    .min(2, { message: 'Name must be at least 2 characters' })
+    .max(50, { message: 'Name must be at most 50 characters' }),
+
+  email: z.string({ required_error: 'Email is required' }).email({ message: 'Email must be a valid email address' }),
+
+  message: z
+    .string({ required_error: 'Message is required' })
+    .min(10, { message: 'Message must be at least 10 characters' })
+    .max(1000, { message: 'Message must be at most 1000 characters' })
 })
