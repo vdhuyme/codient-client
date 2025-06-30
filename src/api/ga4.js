3bfa32cd8b024838b15cc7963ba9bf7eb85839bb
voduchuy2001 <voduchuy2001@gmail.com>
2025-07-01 05:13:08 +0700

    bug: ga4

--- src/api/ga4.js
+++ src/api/ga4.js
@@ -1,6 +1,18 @@
+import { subDays, format, parseISO } from 'date-fns'
 import api from '@/utils/axios'
 
 export const getGA4s = async ({ startAt, endAt }) => {
-  const response = await api.get('/stats/ga4', { params: { startAt, endAt } })
+  const now = new Date()
+  const isBefore9AM = now.getHours() < 9
+
+  const adjustedEndAt = isBefore9AM ? format(subDays(now, 1), 'yyyy-MM-dd') : endAt
+
+  const response = await api.get('/stats/ga4', {
+    params: {
+      startAt,
+      endAt: adjustedEndAt
+    }
+  })
+
   return response.data
 }
